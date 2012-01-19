095a02c01da46328cb2f429415fe3f370eec4fa8
Jan Chęć <kern3l@kern3l.net>
2012-01-19 01:26:41 +0100

    Remove urls.py
    Change-Id: I38e98055e21c18043b04688a443a5d29c10a2d67
    Reviewed-on: http://review.pozytywnie.pl:8080/2324
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>
    Tested-by: Tomasz Wysocki <tomwys@gmail.com>

--- javascript_configuration/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('javascript_configuration.views',
-    url(r'^configuration.js$', 'load_configuration', name='javascript-configuration'),
-)
