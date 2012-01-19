e7c87f3db55af201c696eeddbca12b964d748bb3
Tomasz Wysocki <tomasz@wysocki.info>
2012-01-19 18:13:11 +0100

    Add runtests.py.
    Change-Id: Icaf1da0ce5c713ebd063c089c1eab8987f3f559c
    Reviewed-on: http://review.pozytywnie.pl:8080/2334
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>
    Tested-by: Tomasz Wysocki <tomwys@gmail.com>

---
+++ runtests.py
@@ -0,0 +1,23 @@
+# Based on: https://github.com/zsiciarz/django-envelope/blob/master/runtests.py
+
+import os
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS = (
+            'javascript_configuration',
+        ),
+    )
+
+from django.core.management import call_command
+
+call_command('test', 'javascript_configuration')
+
