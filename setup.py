7eddb14f153601e28b69ce68fd16530537e468a7
Jan Chęć <kern3lly@gmail.com>
2012-03-08 14:28:23 +0100

    Fix setup.py with missing package_data.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(name):
     return open(path.join(path.dirname(__file__), name)).read()
@@ -9,13 +9,11 @@ setup(
     name='django-javascript-settings',
     description="django-javascript-settings is a Django application that provides a way of passing settings for Django applications to JavaScript.",
     long_description=read("README.rst"),
-    version='1.0',
+    version='1.0.1',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
         'django',
     ),
-    packages=[
-        'javascript_settings',
-    ],
+    packages=find_packages(),
 )
